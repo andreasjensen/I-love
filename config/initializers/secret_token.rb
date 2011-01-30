cad76c30ba5d0af08bbe2bac9eb575fd629ce572
Andreas Jensen <andreas.jense@gmail.com>
2011-01-30 21:32:41 +0100

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Love::Application.config.secret_token = '7412d0c0c6506664c108c1eca168f8aa404bcd7853e0e172ea113e44a593ff5098b9a93a5787898fce0666c1dbd90b8bd08c825c02b326cb78e4afd82f3638de'
