7ea5e1c217baacfd3168e9b28b5161853d6a58e8
Andreas Jensen <andreas.jense@gmail.com>
2011-01-30 22:51:08 +0100

    Minor update

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,7 @@
 class Log < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :love
+  validates_presence_of :facebook
+  validates_uniqueness_of :facebook
+  validates_uniqueness_of :twitter
 end
